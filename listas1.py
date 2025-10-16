abd5bd73b0e7ec7d94e6ca20e5432f60f7121a80
Mario Montoya Moreno <alfonsomont8@gamil.com>
2025-10-15 19:46:49 -0600

    Este es un ejemplo de como se usan las listas en Python Se barcan temas de subindices, sublistas, indexación de una lista en otra y combinacion de diferentes tipos de datos dentro de una sola lista

---
+++ listas1.py
@@ -0,0 +1,8 @@
+lista = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+lista2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", 52, 63, 22.75, 52.31, True, [1, 2, 3]]
+
+print(lista)
+print(lista[0])
+print("lista inversa: ", lista[-5])
+print("Sublista ->", lista[:4])
+print("Lista 2 -> ", lista2[5:11])
