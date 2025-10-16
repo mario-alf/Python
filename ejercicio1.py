c3f603cfba2884f416c11fe6a3002650ab22de80
Mario Montoya Moreno <alfonsomont8@gamil.com>
2025-10-15 19:40:42 -0600

    Este es el primer ejercio donde vemos operadores aritmeticos y jerarquia de operaciones

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+a = int(input("Ingresa el primero numero: "))
+b = int(input("Ingresa el segundo numero: "))
+c = int(input("Ingresa el tercer numero: "))
+
+resultado = (a**3 * (b**2 - 2*a*c))/ (2*b)
+
+print(f"El resultado fue: {resultado}")
