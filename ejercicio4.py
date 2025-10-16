1e5d904e9e2c14cb0d09831d020e77dbd0b6c553
Mario Montoya Moreno <alfonsomont8@gamil.com>
2025-10-15 19:42:36 -0600

    Ejercicio donde calculamos el area y longitud de una circufeerencia, Se uso la libreria de math

---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+import math
+
+radio = float(input("Ingresa el radio de la circunferencia: "))
+area = math.pi * (radio**2)
+longitud = 2 * math.pi * radio
+
+print(f"El resultado del area es: {area}")
+print(f"El resultado de la longitud es: {longitud}")
