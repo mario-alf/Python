c07e5d85614e1a491fb81c8224132fd1fd35699a
Mario Montoya Moreno <alfonsomont8@gamil.com>
2025-10-15 19:53:54 -0600

    Se agrego list1 como ejemplo

--- listas2.py
+++ listas2.py
@@ -1,3 +1,4 @@
 lista2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", 52, 63, 22.75, 52.31, True, [1, 2, 3]]
-
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(len(lista2))
+print(len(list1))
