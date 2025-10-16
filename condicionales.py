add0685f714851752ec59a667f3d8cb6e510a758
Mario Montoya Moreno <alfonsomont8@gamil.com>
2025-10-15 19:39:10 -0600

    Este es un código ejemplo del uso de condicionales

---
+++ condicionales.py
@@ -0,0 +1,9 @@
+num = int(input("Ingrese un numero: "))
+
+if num > 0:
+    print("El numero es positivo")
+elif num == 0:
+    print("El numero es igual a cero")
+else:
+    print("El numero es negativo")
+
